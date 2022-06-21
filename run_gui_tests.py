21b2d80ca8e673417ccde1b598656145dd5b462c
Daniel Zimmermann <dzimmer@fzi.de>
2022-06-21 21:21:39 +0200

    Using manual server numbers for xvfb

--- run_gui_tests.py
+++ run_gui_tests.py
@@ -3,11 +3,12 @@ import subprocess
 
 
 def test_run(number):
-    return subprocess.call(['xvfb-run', '-a', 'mvn', 'surefire:test'], shell=False)
+    return subprocess.call(['xvfb-run', '-n', str(number+99), 'mvn', 'surefire:test'], shell=False)
 
 
 if __name__ == '__main__':
     count = multiprocessing.cpu_count()
+    count = 40
     pool = multiprocessing.Pool(processes=count)
     pool.map(test_run, [i for i in range(count)])
 
